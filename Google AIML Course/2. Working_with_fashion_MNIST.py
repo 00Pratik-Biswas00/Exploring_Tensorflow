9051897686a768fce7cde866b5f675e630924ef8
Pratik Biswas <04biswaspratik@gmail.com>
2024-05-04 19:53:19 +0530

    2. Working_with_fashion_MNIST

---
+++ Google AIML Course/2. Working_with_fashion_MNIST.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+print(tf.__version__)
+
+mnist = tf.keras.datasets.fashion_mnist
+
+# Calling load_data on that object gives you two sets of two lists: training values and testing values, which represent graphics that show clothing items and their labels.
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+
+import matplotlib.pyplot as plt
+plt.imshow(training_images[0])
+print(training_labels[0])
+print(training_images[0])
+
+# You'll notice that all the values are integers between 0 and 255. When training a neural network, it's easier to treat all values as between 0 and 1, a process called normalization. Fortunately, Python provides an easy way to normalize a list like that without looping.
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+# Sequential defines a sequence of layers in the neural network.
+# Flatten takes a square and turns it into a one-dimensional vector.
+# Dense adds a layer of neurons.
+# Activation functions tell each layer of neurons what to do. There are lots of options, but use these for now:
+# Relu effectively means that if X is greater than 0 return X, else return 0. It only passes values of 0 or greater to the next layer in the network.
+# Softmax takes a set of values, and effectively picks the biggest one. For example, if the output of the last layer looks like [0.1, 0.1, 0.05, 0.1, 9.5, 0.1, 0.05, 0.05, 0.05], then it saves you from having to sort for the largest value—it returns [0,0,0,0,1,0,0,0,0].
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# The use of metrics= as a parameter, which allows TensorFlow to report on the accuracy of the training by checking the predicted results against the known answers
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=5)
+
+
+model.evaluate(test_images, test_labels)
