a3a3ec05f9b263fd0b708760d7cd81a0c7489a46
Pratik Biswas <04biswaspratik@gmail.com>
2024-05-05 11:20:58 +0530

    chapter 1 is updated

---
+++ Goggle AIML Course/Chapter 1/3. Build_convolution_perform_poolimg.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+from scipy import misc
+i = misc.ascent()
+
+import matplotlib.pyplot as plt
+plt.grid(False)
+plt.gray()
+plt.axis('off')
+plt.imshow(i)
+plt.show()
+
+i_transformed = np.copy(i)
+size_x = i_transformed.shape[0]
+size_y = i_transformed.shape[1]
+
+# This filter detects edges nicely
+# It creates a filter that only passes through sharp edges and straight lines. 
+filter = [ [-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+# If all the digits in the filter don't add up to 0 or 1, you 
+# should probably do a weight to get it to do so
+# so, for example, if your weights are 1,1,1 1,2,1 1,1,1
+# They add up to 10, so you would set a weight of .1 if you want to normalize them
+weight  = 1
+
+for x in range(1,size_x-1):
+  for y in range(1,size_y-1):
+      output_pixel = 0.0
+      output_pixel = output_pixel + (i[x - 1, y-1] * filter[0][0])
+      output_pixel = output_pixel + (i[x, y-1] * filter[0][1])
+      output_pixel = output_pixel + (i[x + 1, y-1] * filter[0][2])
+      output_pixel = output_pixel + (i[x-1, y] * filter[1][0])
+      output_pixel = output_pixel + (i[x, y] * filter[1][1])
+      output_pixel = output_pixel + (i[x+1, y] * filter[1][2])
+      output_pixel = output_pixel + (i[x-1, y+1] * filter[2][0])
+      output_pixel = output_pixel + (i[x, y+1] * filter[2][1])
+      output_pixel = output_pixel + (i[x+1, y+1] * filter[2][2])
+      output_pixel = output_pixel * weight
+      if(output_pixel<0):
+        output_pixel=0
+      if(output_pixel>255):
+        output_pixel=255
+      i_transformed[x, y] = output_pixel
+      
+      
+# Plot the image. Note the size of the axes -- they are 512 by 512
+plt.gray()
+plt.grid(False)
+plt.imshow(i_transformed)
+#plt.axis('off')
+plt.show()   
+
+# There are a number of different types of pooling, but you'll use one called Maximum (Max) Pooling.
+
+# Iterate over the image and, at each point, consider the pixel and its immediate neighbors to the right, beneath, and right-beneath. Take the largest of those (hence max pooling) and load it into the new image. Thus, the new image will be one-fourth the size of the old.
+
+new_x = int(size_x/2)
+new_y = int(size_y/2)
+newImage = np.zeros((new_x, new_y))
+for x in range(0, size_x, 2):
+  for y in range(0, size_y, 2):
+    pixels = []
+    pixels.append(i_transformed[x, y])
+    pixels.append(i_transformed[x+1, y])
+    pixels.append(i_transformed[x, y+1])
+    pixels.append(i_transformed[x+1, y+1])
+    pixels.sort(reverse=True)
+    newImage[int(x/2),int(y/2)] = pixels[0]
+ 
+# Plot the image. Note the size of the axes -- now 256 pixels instead of 512
+plt.gray()
+plt.grid(False)
+plt.imshow(newImage)
+#plt.axis('off')
+plt.show()
