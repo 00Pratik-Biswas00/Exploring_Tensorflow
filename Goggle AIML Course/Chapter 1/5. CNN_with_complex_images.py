622fd52f8101a654ca3f2fdf8e180710456f0f09
Pratik Biswas <04biswaspratik@gmail.com>
2024-05-05 11:27:40 +0530

    5. CNN_with_complex_images

--- Goggle AIML Course/Chapter 1/5. CNN_with_complex_images.py
+++ Goggle AIML Course/Chapter 1/5. CNN_with_complex_images.py
@@ -6,10 +6,10 @@ zip_ref = zipfile.ZipFile(local_zip, 'r')
 zip_ref.extractall('horse-or-human')
 zip_ref.close()
 
-# Directory with our training horse pictures
+# Directory with our training horse pictures.
 train_horse_dir = os.path.join('horse-or-human/horses')
  
-# Directory with our training human pictures
+# Directory with our training human pictures.
 train_human_dir = os.path.join('horse-or-human/humans')
 
 train_horse_names = os.listdir(train_horse_dir)
